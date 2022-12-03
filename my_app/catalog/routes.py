6fffba977e15645d01a3b0962ab9694a7ec8ace8
Paulina Morales <99380026+helue23@users.noreply.github.com>
2022-12-03 15:41:51 -0600

    Update routes.py

--- my_app/catalog/routes.py
+++ my_app/catalog/routes.py
@@ -140,10 +140,26 @@ def create_ticket():
         choice = Menu.query.filter_by(item_name=order[items]).first()
         if choice:
             ticket.menu.append(choice)
-            
+
     db.session.add(ticket)
     db.session.commit()
     return 'Customer ticket created'
+
+#create sum of customer ticket
+@catalog.route('/calculate-ticket-sum', methods=['POST',])
+def calculate_ticket():
+    current_id = request.json.get('id')
+    sum = 0
+    st = "Food Purchase Receipt\n\n"
+    result = db.session.query(Menu).join(Menu.customer_ticket).filter(CustomerTicket.id==current_id).all()
+    if result:
+        for item in result:
+            st = st + str(item.item_name) + '   $' + str('%.2f' % item.price) + '\n'
+            sum = item.price + sum
+        sum = ('%.2f' % sum)
+        st = st + 'Your total for today is: $' + str(sum)
+        return st
+    return 'Ticket not found'
 
 
 
